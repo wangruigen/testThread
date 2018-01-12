9598fa6235f896f2360c047000a528685e898908
LucianCarry <1028725142@qq.com>
2018-01-12 17:22:22 +0800

    第一章

--- src/core/testThread/PackageInfo.java
+++ src/core/testThread/PackageInfo.java
@@ -1,6 +1,11 @@
 package core.testThread;
 /**
  * core.testThread
+ * <h1>第一章</h1>
+ * <h1>本章小结：</h1>
+ * <p>本章介绍了Thread类的api,在使用这些api的过程中，会出现一些意想不到的情况，其实这也是
+ * 多线程具有不可预知性的一个体现。学习和掌握这些常用情况，也就掌握多线程开发的命脉与习性
+ * ，是学习多线程更深层知道的基础。</p>
  * @author wrg
  */
 public class PackageInfo{
