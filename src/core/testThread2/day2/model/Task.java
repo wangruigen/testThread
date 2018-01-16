48f0864245013ebefea3cdf79ade15e630889627
LucianCarry <1028725142@qq.com>
2018-01-16 14:20:58 +0800

    Object对象做为synchronized(非this)锁

--- src/core/testThread2/day2/model/Task.java
+++ src/core/testThread2/day2/model/Task.java
@@ -3,7 +3,7 @@ package core.testThread2.day2.model;
 public class Task {
 	private String name;
 	private String password;
-	private String lock = new String();
+	private Object lock = new Object();
 	public Task() {
 		super();
 	}
@@ -27,23 +27,23 @@ public class Task {
 	public void method(Task task){
 		/**
 		 * 锁定一个对象：
-		
+		 */
 		synchronized (lock) {//多线程运行Task类中的方法时，使用Task是一个实例的情况，运行core.testThread2.day2.model.TaskTest main方法时打印是同步的。
 			for (int i = 0; i < 100; i++) {
 				System.out.println("thread="+Thread.currentThread().getName()+" name="+task.getName()+" password="+task.getPassword());
 			}
 		}
-		 */
+		
 		/**
 		 * 锁定非同一个对象：
-		*/
-		String lock2 = new String();
+		
+		Object lock2 = new Object();
 		synchronized (lock2) {//每个线程进来都是创建一个lock2对象，运行core.testThread2.day2.model.TaskTest main方法时打印是异步的。
 			for (int i = 0; i < 100; i++) {
 				System.out.println("thread="+Thread.currentThread().getName()+" name="+task.getName()+" password="+task.getPassword());
 			}
 		}
-		 
+		 */
 		
 	}
 }
